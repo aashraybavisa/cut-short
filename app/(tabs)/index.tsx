6391947804954251a29e447ff6edc6d865ec094e
aashraybavisa <aashray1042@gmail.com>
2024-11-30 20:59:19 -0500

    fix: container configured

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, useColorScheme } from "react-native";
 import React from "react";
+//
 import AppContainer from "@/components/AppContainer";
+import { Colors } from "@/constants/Colors";
 
 const HomeTabScreen = () => {
+  const colorScheme = useColorScheme() ?? "light";
   return (
-    <AppContainer>
+    <AppContainer
+      backgroundColor={Colors[colorScheme]?.background}
+      isTopSafeArea
+    >
       <Text>HomeTabScreen</Text>
     </AppContainer>
   );
